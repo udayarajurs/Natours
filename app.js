5cea15a498ab0c65e0e17ad97c59ffc9143b2934
udaya raj urs G <Udayarajurs7@gmail.com>
2022-08-26 18:19:52 +0530

    working on

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-
+console.log(process.env.NODE_ENV);
 // 1) MIDDLEWARES
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
