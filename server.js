a83d0ac213fd121da83d53651e633f40b53594dc
udaya raj urs G <Udayarajurs7@gmail.com>
2022-08-27 12:08:08 +0530

    working on

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
-// console.log(app.get('env'));
-// console.log(process.env);
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB cooncatoin successful'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
