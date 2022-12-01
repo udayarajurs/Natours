a7a17905718af42e13b79405fd163c5d73954c29
Udaya raj urs <udayarajurs7@gmail.com>
2022-12-01 23:34:29 +0530

    working

--- models/tourModel.js
+++ models/tourModel.js
@@ -64,6 +64,11 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function () {
+  console.log(this);
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
