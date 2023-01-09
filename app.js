838e75b80965d35c58f238c2d8aba71d2b11b767
udaya raj urs G <Udayarajurs7@gmail.com>
2023-01-09 16:28:58 +0530

    working on 116

--- app.js
+++ app.js
@@ -17,12 +17,6 @@ if (process.env.NODE_ENV === 'development') {
 // get UI desgin url
 app.use(express.static(`${__dirname}/public`));
 
-// middleware
-app.use((req, res, next) => {
-  console.log('hello from the middleware...');
-  next();
-});
-
 // get current time  (middleware)
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -32,5 +26,28 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'Fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+// ERROR HANDLING MIDDLEWARE
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
